7286693f470ea19608f21536b7622fe2da20b762
Elias Tokola <eliastokola88@gmail.com>
2023-06-05 12:58:38 -0400

    updated cors options

--- server.js
+++ server.js
@@ -8,23 +8,30 @@ require('dotenv').config();
 
 const jwt = require('jsonwebtoken');
 
-app.use(cors());
+const corsOptions ={
+    origin:'https://bookq.netlify.app', 
+    credentials:true,
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    if ('OPTIONS' == req.method) {
-        return res.sendStatus(200);
-    } else {
-        next();
-    }
-});
+//     if ('OPTIONS' == req.method) {
+//         return res.sendStatus(200);
+//     } else {
+//         next();
+//     }
+// });
 
 app.get("/check", (req, res) => {
     const sqlSelect = "SELECT * FROM user_books";
