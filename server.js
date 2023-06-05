8da662cdc4646d3f4014a800b9758a7443964291
Elias Tokola <eliastokola88@gmail.com>
2023-06-05 18:13:11 -0400

    changed cors origin to test on local machine

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const corsOptions ={
-    origin:'https://bookq.netlify.app', 
+    origin:'https://http://localhost:3000', 
     credentials:true,
     optionSuccessStatus:200
 }
